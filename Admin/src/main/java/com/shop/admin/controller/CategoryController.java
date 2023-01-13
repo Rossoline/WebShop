cb4dc782ae0957ddd8518267fecd4f907cfdfc54
Ilja Nekrituk <Nekrit99@gmail.com>
2023-01-13 17:03:06 +0200

    categories.html

--- Admin/src/main/java/com/shop/admin/controller/CategoryController.java
+++ Admin/src/main/java/com/shop/admin/controller/CategoryController.java
@@ -1,15 +1,17 @@
 package com.shop.admin.controller;
 
+
 import com.shop.library.model.Category;
 import com.shop.library.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
@@ -18,7 +20,10 @@ public class CategoryController {
     private CategoryService categoryService;
 
     @GetMapping("/categories")
-    public String categories(Model model) {
+    public String categories(Model model, Principal principal) {
+        if(principal==null){
+            return "redirect:/login";
+        }
         List<Category> categories = categoryService.findAll();
         model.addAttribute("categories", categories);
         model.addAttribute("size", categories.size());
@@ -28,16 +33,17 @@ public class CategoryController {
     }
 
     @PostMapping("/add-category")
-    public String add(@ModelAttribute("categoryNew") Category category, RedirectAttributes attributes){
-        try{
+    public String add(@ModelAttribute("categoryNew") Category category, RedirectAttributes attributes) {
+        try {
             categoryService.save(category);
-            attributes.addFlashAttribute("success","Added successfully");
-        }catch (Exception e) {
-            //TODO change stackTrace
+            attributes.addFlashAttribute("success", "Added successfully");
+        } catch (DataIntegrityViolationException e) {
             e.printStackTrace();
-            attributes.addFlashAttribute("failed", "Failed");
+            attributes.addFlashAttribute("failed", "Failed to add because duplicate name");
+        } catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Error server");
         }
         return "redirect:/categories";
-
     }
 }
