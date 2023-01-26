5e7c45c4c6d768daef91c791ca0424d43c77fb75
Ilja Nekrituk <Nekrit99@gmail.com>
2023-01-27 00:09:15 +0200

    Created module Customer add all models for Customer

--- Customer/src/main/java/com/shop/customer/CustomerApplication.java
+++ Customer/src/main/java/com/shop/customer/CustomerApplication.java
@@ -1,16 +1,17 @@
-package com.shop.admin;
+package com.shop.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.shop.library.*", "com.shop.admin.*"})
+@SpringBootApplication(scanBasePackages = "com.shop.customer")
 @EnableJpaRepositories(value = "com.shop.library.repository")
 @EntityScan(value = "com.shop.library.model")
-public class AdminApplication {
+public class CustomerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+        SpringApplication.run(CustomerApplication.class, args);
     }
+
 }
---
+++ Library/src/main/java/com/shop/library/model/City.java
@@ -0,0 +1,19 @@
+package com.shop.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "cities")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private Long id;
+    private String name;
+}
