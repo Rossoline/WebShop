dd5f1f3af5d11fabee4e484b6899c4a84258e370
Ilja Nekrituk <Nekrit99@gmail.com>
2023-03-09 22:45:25 +0200

    pom

--- Customer/src/main/java/com/shop/customer/CustomerApplication.java
+++ Customer/src/main/java/com/shop/customer/CustomerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.shop.customer.*", "com.shop.library.*"})
+@SpringBootApplication(scanBasePackages = {"com.shop.library.*","com.shop.customer.*"})
 @EnableJpaRepositories(value = "com.shop.library.repository")
 @EntityScan(value = "com.shop.library.model")
 public class CustomerApplication {
