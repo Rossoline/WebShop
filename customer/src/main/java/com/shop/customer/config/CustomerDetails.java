1423664c8919a3340bc6d7f019526ba3652ed3c9
Ilja Nekrituk <Nekrit99@gmail.com>
2023-03-14 13:23:20 +0200

    customer

---
+++ customer/src/main/java/com/shop/customer/config/CustomerDetails.java
@@ -0,0 +1,51 @@
+package com.shop.customer.config;
+
+import com.shop.library.model.Customer;
+import com.shop.library.model.enums.Role;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomerDetails implements UserDetails {
+    private Customer customer;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(Role.CUSTOMER.toString()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return customer.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return customer.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
